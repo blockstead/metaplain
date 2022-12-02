04e147bb30a7ae3302ae536a62d70f3eb04a51f1
Dr. Nobel Khandaker <nobel@huskers.unl.edu>
2022-12-02 16:16:59 +0600

    Added logging

--- app/main.py
+++ app/main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
+import logging
+import logging.config
+import yaml
+
+# logger
+with open('./app/loggingConfig.yaml','r') as configFile:
+    config=yaml.safe_load(configFile.read())
+    logging.config.dictConfig(config)
+
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
+    logger.debug('sample log message')
     return {"message": "Hello World"}
 
 
